afb44de4cd34eb1deff65feaa0c5d3c4595e785b
bingcheng <bingcheng@Ctrip.com>
2018-06-17 18:34:50 +0800

    控件使用Demo完善

--- Example/App.js
+++ Example/App.js
@@ -6,53 +6,76 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    ScrollView,
+    Dimensions,
+    Text,
+    View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import PageableScrollView from 'react-native-pageable-scrollview';
+
+const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get('window');
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    constructor(props){
+        super(props);
+
+        let { initPage = 0, containerPadding = 10 } = (props || {});
+        initPage = initPage <=0 ? 0 : initPage;
+
+
+        this.state = {
+            pageNum: 5,
+            pageIndex: initPage,
+            containerWidth: SCREEN_WIDTH - containerPadding * 2,
+            containerPadding: containerPadding
+        }
+
+    }
+
+    componentDidUpdate(){
+
+    }
+    componentDidMount(){
+
+    }
+
+    render() {
+
+        return (<PageableScrollView style={[styles.scroll]} contentStyle={[styles.page]}>
+            <View style={[styles.content]}><Text style={[styles.normal]}>The first page</Text></View>
+            <View style={[styles.content]}><Text style={[styles.normal]}>The second page</Text></View>
+            <View style={[styles.content]}><Text style={[styles.normal]}>The third page</Text></View>
+            <View style={[styles.content]}><Text style={[styles.normal]}>The fourth page</Text></View>
+            <View style={[styles.content]}><Text style={[styles.normal]}>The fifth page</Text></View>
+        </PageableScrollView>)
+    }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    scroll: {
+        flex: 1,
+        backgroundColor: '#F2F2F2',
+        marginTop: 30,
+    },
+    page: {
+        height: 100
+    },
+    beginning: {
+        marginLeft: 20,
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#18A0F0'
+
+    },
+    normal: {
+        color: '#FFF'
+    }
 });
